20067ad069ccca1339ee47b00993a75d82bf728b
Alex <barnukoff@gmail.com>
2018-08-05 11:47:10 +0900

    Initial branch compiled without errors

---
+++ desktop/src/com/mygdx/mygdxexample/desktop/DesktopLauncher.java
@@ -0,0 +1,13 @@
+package com.mygdx.mygdxexample.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.mygdx.mygdxexample.MyGdxExample;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		System.setProperty("org.lwjgl.opengl.Display.allowSoftwareOpenGL", "true");
+		new LwjglApplication(new MyGdxExample(), config);
+	}
+}
